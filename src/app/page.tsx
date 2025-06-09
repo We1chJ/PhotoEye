274be1d1c0b0c05039df889ba4bdfa6415af67e0
Welch <weq511762338@gmail.com>
2025-06-08 22:59:04 -0400

    added more stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
 'use client'
 import Image from "next/image";
 import ShapeBlur from "@/components/ShapeBlur";
+import { ModeToggle } from "@/components/ModeToggle";
+import Magnet from "@/components/Magnet";
+import { Button } from "@/components/ui/button";
+import { Link, Star } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="w-screen h-screen px-64">
-      <div className="py-4 flex items-center">
-        <div className="ml-2 w-20 h-20 overflow-hidden rounded-full inline-block align-middle">
-          <Image
-            src="/photoeye.png"
-            alt="PhotoEye Logo"
-            width={128}
-            height={128}
-            className="object-cover w-full h-full"
-          />
+    <div className="w-screen h-screen px-60">
+      <div className="py-2 flex items-center justify-between">
+        <div className="flex items-center">
+          <div className="w-20 h-20 overflow-hidden rounded-full inline-block align-middle">
+            <Image
+              src="/photoeye.png"
+              alt="PhotoEye Logo"
+              width={128}
+              height={128}
+              className="object-cover w-full h-full"
+            />
+          </div>
+          <span className="ml-4 text-3xl font-extrabold tracking-tight bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-mono">
+            PhotoEye
+          </span>
         </div>
-        <span className="text-3xl font-extrabold tracking-tight bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent font-mono">
-          PhotoEye
-        </span>
+        <div className="flex items-center space-x-4">
+          <Magnet padding={200} disabled={false} magnetStrength={80}>
+            <a href="https://github.com/We1chJ/PhotoEye" target="_blank" rel="noopener noreferrer">
+              <Button variant="outline">
+                Star on GitHub
+                <Star />
+              </Button>
+            </a>
+          </Magnet>
+          <ModeToggle />
+        </div>
       </div>
 
       <div style={{ position: 'relative', height: '500px', overflow: 'hidden' }}>
